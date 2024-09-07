7ebb5e509fe3a276b3a0f9475ed32fb4ead3e551
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-09-06 19:15:05 -0700

    Atatus-spa
    Atatus-spa added to dependencies

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('021a4040935648aab43baca84c1df4ed').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
