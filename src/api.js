14e2e4790f5f8fa4fafcd15af74b3bb72af16fa4
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-09-08 18:22:48 -0700

    Offline Capability

--- src/api.js
+++ src/api.js
@@ -43,8 +43,16 @@ export const extractLocations = (events) => {
 };
  
 export const getEvents = async () => {
+  //NProgress.start();
+
   if (window.location.href.startsWith("http://localhost")) {
     return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    //NProgress.done();
+    return events?JSON.parse(events):[];
   }
 
   const token = await getAccessToken();
@@ -55,6 +63,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      //NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
