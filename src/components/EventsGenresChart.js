f16a1bc9a464d49c6a80bccdc6ef5f500c27db97
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2025-02-18 13:45:32 -0800

    Update EventsGenresChart.js

--- src/components/EventsGenresChart.js
+++ src/components/EventsGenresChart.js
@@ -57,7 +57,7 @@ const EventGenresChart = ({ events }) => {
               fill="#8884d8"
               labelLine={false}
               label={renderCustomizedLabel}
-              outerRadius={100}           
+              outerRadius={130}           
             >
               {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index]} />
