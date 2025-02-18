53ebb52fc184107d24182e664d20fc9cd4f2fcc1
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2025-02-18 13:37:36 -0800

    Pie Chart Radius Resize

--- src/components/EventsGenresChart.js
+++ src/components/EventsGenresChart.js
@@ -57,7 +57,7 @@ const EventGenresChart = ({ events }) => {
               fill="#8884d8"
               labelLine={false}
               label={renderCustomizedLabel}
-              outerRadius={120}           
+              outerRadius={100}           
             >
               {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={colors[index]} />
